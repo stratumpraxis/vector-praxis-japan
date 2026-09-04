20e42eb4b53e25f7f15282494039eeae26b144fa
stratumpraxis <stratumpraxis@gmail.com>
2026-09-04 22:04:13 +0900

    feat(gwr): add fail-closed revenue partner config

---
+++ incubator/global-work-radar/revenue-config.js
@@ -0,0 +1,7 @@
+window.GWR_REVENUE = Object.freeze({
+  enabled: false,
+  partner: 'Deel',
+  affiliateUrl: '',
+  disclosure: 'このリンク経由で申込みが成立した場合、Global Work Radarが紹介料を受け取ることがあります。',
+  campaign: 'gwr_deel_2026_09'
+});
